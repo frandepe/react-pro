34f3c3a541a73f25c2a1bcad152e9d368f25eecd
Franco De Paulo <frandepaulo@yahoo.com.ar>
2022-09-17 15:34:56 -0300

    Component compound ready

---
+++ src/02-component-patterns/components/ProductCard.tsx
@@ -0,0 +1,31 @@
+import { useProducts } from "../hooks/useProducts";
+import styles from "../styles/styles.module.css";
+import { createContext } from "react";
+import {
+  ProductCardProps,
+  ProductContextProps,
+} from "../interfaces/interfaces";
+
+export const ProductContext = createContext({} as ProductContextProps);
+const { Provider } = ProductContext;
+
+export const ProductCard = ({ children, product }: ProductCardProps) => {
+  const { counter, increaseBy } = useProducts();
+
+  return (
+    <Provider
+      value={{
+        counter,
+        increaseBy,
+        product,
+      }}
+    >
+      <div className={styles.productCard}>
+        {children}
+        {/* <ProductImage img={product.img} />
+      <ProductTitle title={product.title} />
+      <ProductButtons counter={counter} increaseBy={increaseBy} /> */}
+      </div>
+    </Provider>
+  );
+};
--- src/routes/Navigation.tsx
+++ src/routes/Navigation.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter } from "react-router-dom";
-import { Routes, Route, NavLink, Navigate } from "react-router-dom";
+import { Routes, Route, NavLink } from "react-router-dom";
+import { ShoppingPage } from "../02-component-patterns/pages/ShoppingPage";
 import logo from "../logo.svg";
 
 export const Navigation = () => {
@@ -7,7 +8,7 @@ export const Navigation = () => {
     <BrowserRouter>
       <div className="main-layout">
         <nav>
-          <img src={logo} alt="React Loco" />
+          <img src={logo} alt="React Logo" />
           <ul>
             <li>
               <NavLink
@@ -27,20 +28,20 @@ export const Navigation = () => {
             </li>
             <li>
               <NavLink
-                to="/users"
+                to="/"
                 className={({ isActive }) => (isActive ? "nav-active" : "")}
               >
-                Users
+                Shopping Page
               </NavLink>
             </li>
           </ul>
         </nav>
 
         <Routes>
-          <Route path="about" element={<h1>About Page</h1>} />
-          <Route path="users" element={<h1>Users Page</h1>} />
           <Route path="/home" element={<h1>Home Page</h1>} />
-          <Route path="/*" element={<Navigate to="/home" replace />} />
+          <Route path="/about" element={<h1>About Page</h1>} />
+          <Route path="/" element={<ShoppingPage />} />
+          {/* <Route path="/*" element={<Navigate to="/home" replace />} /> */}
         </Routes>
       </div>
     </BrowserRouter>
